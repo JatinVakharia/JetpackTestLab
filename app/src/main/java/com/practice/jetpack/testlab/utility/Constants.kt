7552e268dcd9de0d9bd4120d7b887842b9045655
Jatin Jawahar Vakharia <vakhariajat@gmail.com>
2020-06-08 11:37:12 +0530

    Updated changes

--- app/src/main/java/com/practice/jetpack/testlab/utility/Constants.kt
+++ app/src/main/java/com/practice/jetpack/testlab/utility/Constants.kt
@@ -3,8 +3,8 @@ package com.practice.jetpack.testlab.utility
 
 class Constants {
     companion object {
-        const val validUsername = "test@worldofplay.in"
-        const val validPassword = "Worldofplay@2020"
+        const val validUsername = "test@test.in"
+        const val validPassword = "Test@2020"
         const val SUCCESS_CODE = 200
         const val INCORRECT_CODE = 401
         const val INVALID_CODE = 400
